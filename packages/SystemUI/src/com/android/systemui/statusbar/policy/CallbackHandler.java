syberia-project/platform_frameworks_base

StatusBar: Add IMS icon for Android 10
ported from CAF LA.UM.8.3.r1-05800-sdm845.0 tag

Signed-off-by: DennySPB <dennyspb@gmail.com>
Change-Id: I48f782fc766440c8a838eb1df9265c8f9f2ccf0d
Signed-off-by: DennySPB <dennyspb@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/policy/CallbackHandler.java
@@ -125,14 +125,14 @@ public class CallbackHandler extends Handler implements EmergencyListener, Signa
     @Override
     public void setMobileDataIndicators(final IconState statusIcon, final IconState qsIcon,
             final int statusType, final int qsType,final boolean activityIn,
-            final boolean activityOut, final String typeContentDescription,
+            final boolean activityOut, final int volteIcon, final String typeContentDescription,
             final String description, final boolean isWide, final int subId, boolean roaming) {
         post(new Runnable() {
             @Override
             public void run() {
                 for (SignalCallback signalCluster : mSignalCallbacks) {
                     signalCluster.setMobileDataIndicators(statusIcon, qsIcon, statusType, qsType,
-                            activityIn, activityOut, typeContentDescription, description, isWide,
+                            activityIn, activityOut, volteIcon, typeContentDescription, description, isWide,
                             subId, roaming);
                 }
             }

pxnoswip/frameworks_base

Signed-off-by: drpradsms <dr.pradsms@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/policy/CallbackHandler.java
@@ -122,13 +122,13 @@ public class CallbackHandler extends Handler implements EmergencyListener, Signa
     @Override
     public void setMobileDataIndicators(final IconState statusIcon, final IconState qsIcon,
             final int statusType, final int qsType, final boolean activityIn,
-            final boolean activityOut, final CharSequence typeContentDescription,
+            final boolean activityOut, final int volteIcon, final CharSequence typeContentDescription,
             CharSequence typeContentDescriptionHtml, final CharSequence description,
             final boolean isWide, final int subId, boolean roaming) {
         post(() -> {
             for (SignalCallback signalCluster : mSignalCallbacks) {
                 signalCluster.setMobileDataIndicators(statusIcon, qsIcon, statusType, qsType,
-                        activityIn, activityOut, typeContentDescription,
+                        activityIn, activityOut, volteIcon, typeContentDescription,
                         typeContentDescriptionHtml, description, isWide, subId, roaming);
             }
         });