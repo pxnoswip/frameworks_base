5875871b516ce1a90db25d3ce1373806e6fc86cc
drpradsms <dr.pradsms@gmail.com>
2019-12-05 09:10:10 +0530

    dataswitch
    Signed-off-by: drpradsms <dr.pradsms@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
@@ -194,7 +194,7 @@ public class DataSwitchTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.EVO_SETTINGS;
+        return MetricsEvent.NITROGEN_SETTINGS;
     }
 
     @Override
