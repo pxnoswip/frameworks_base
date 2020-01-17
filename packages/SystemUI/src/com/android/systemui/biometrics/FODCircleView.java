0ee79625026d74d806c94a49e92b6a406ed86f1f
XiNGRZ <chenxingyu92@gmail.com>
2020-01-17 12:59:06 +0800

    FODCircleView: add a transparent icon for pressed state
    * For being overlaid by those devices which were calibrated to use
      non-solid color icons
    
    Change-Id: Ia44def94b419803e2d17f2dba49bfcaf19eec8a2

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -315,7 +315,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         updateAlpha();
 
         mPaintFingerprint.setColor(mColor);
-        setImageDrawable(null);
+        setImageResource(R.drawable.fod_icon_pressed);
         invalidate();
     }
 
