30c35fc78da5a7e5b3f46231d7ed22e8dc017f98
drpradsms <dr.pradsms@gmail.com>
2020-01-18 03:19:43 -0800

    keyguardcallbacks

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -2716,15 +2716,4 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
         }
         return mPulsing;
     }
-
-    public boolean setPulsing(boolean pulsing) {
-        mPulsing = pulsing;
-        for (int i = 0; i < mCallbacks.size(); i++) {
-            KeyguardUpdateMonitorCallback cb = mCallbacks.get(i).get();
-            if (cb != null) {
-                cb.onPulsing(mPulsing);
-            }
-        }
-        return mPulsing;
-    }
 }
